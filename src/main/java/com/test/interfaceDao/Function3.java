6add8f8bb0ad4435a00c2eb1923d36d88c297ae3
wangfuqiang <qq2968950775>
2021-11-14 22:04:21 +0800

    第二章：高级函数特性

--- src/main/java/com/test/interfaceDao/Function3.java
+++ src/main/java/com/test/interfaceDao/Function3.java
@@ -2,11 +2,15 @@ package com.test.interfaceDao;
 
 import com.test.t2.Function2;
 
+import java.util.function.Function;
+
 /**
  * 多态函数
  */
 public interface Function3<T,U> {
     Function3<T,Function3<T,T>> apply(Function3<T,Function3<T,T>>  arg);
 
-    <T,U,V> Function3<Function3<T,U>,Function3<Function3<V,T>,Function3<V,U>>> compose();
+    static <T, U, V> Function<Function<U, V>, Function<Function<T, U>, Function<T, V>>> higherCompose() {
+        return f -> g -> x -> f.apply(g.apply(x));
+    }
 }
